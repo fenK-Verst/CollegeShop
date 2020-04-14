df6de3e430d05fff361cebfff365f21a1523a6ef
fenK <mihha1818@gmail.com>
2020-04-14 14:59:26 +0600

    add: twig

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+use Twig\Loader\FilesystemLoader;
+
+define("PROJECT_DIR", __DIR__."/..");
+require_once (PROJECT_DIR."/vendor/autoload.php");
+
+$loader = new FilesystemLoader(PROJECT_DIR.'/templates');
+$twig = new Environment($loader, [
+    'cache' => PROJECT_DIR.'/var/cache',
+]);
+
+try {
+    echo $twig->render('base.html.twig', ['name' => 'Fabien']);
+} catch (LoaderError $e) {
+} catch (RuntimeError $e) {
+} catch (SyntaxError $e) {
+}
+
