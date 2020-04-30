6949062ea2cf27359bdcb22d0fdbd1e5a97e506f
fenK <mihha1818@gmail.com>
2020-04-30 17:15:24 +0600

    fix

--- src/Db/ArrayDataManager.php
+++ src/Db/ArrayDataManager.php
@@ -18,7 +18,6 @@ class ArrayDataManager implements Interfaces\ArrayDataManagerInterface
 
     public function query(string $query)
     {
-        var_dump($query);
         $result = $this->connection->query($query);
         if ($this->connection->errno) {
             $error = $this->connection->error;
