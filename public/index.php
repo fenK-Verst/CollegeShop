cef2834def7ec3d724afcfc619b0c9779a8117f7
Сюрсин Михаил <mixail.syursin@gmail.com>
2020-11-04 17:10:02 +0600

    Все убрано в докер

--- public/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once $_SERVER['DOCUMENT_ROOT']."/../src/bootstrap.php";
-
-$kernel->run();
---
+++ shop/public/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT']."/../src/bootstrap.php";
+
+$kernel->run();
